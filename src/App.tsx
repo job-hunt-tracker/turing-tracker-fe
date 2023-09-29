5b9b08bf1f0bd7fbacb2a670917e68dca6edbe96
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-09-29 14:43:27 -0600

    Add state management, hooks, and example buttons to App for triggering the open of the new application  and application details dialogs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from '@mui/material';
 import './App.css';
+import AppDialog from './dialogs/AppDialog';
+import useDialog from './dialogs/dialogHooks';
 
 function App() {
+  const { openDialog } = useDialog();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="outlined" color="primary" onClick={() => openDialog('new')}>
+          Open New App Dialog
+        </Button>
+        <Button variant="outlined" color="primary" onClick={() => openDialog('details')}>
+          Open App Details Dialog
+        </Button>
+        <AppDialog />
         <a
           className="App-link"
           href="https://reactjs.org"
