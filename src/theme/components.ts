f076c166ed6f7b398e1859d25f9c6a67fc2a043a
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-09-29 13:27:35 -0600

    Add initial set up for Material UI themeing (with placeholder styles/colors) and add React Recoil for GSM.

---
+++ src/theme/components.ts
@@ -0,0 +1,27 @@
+// styling overrides for the standard MUI components
+
+import { type Palette, type ThemeOptions } from "@mui/material/styles";
+
+export const components = (palette: Palette): ThemeOptions["components"] => ({
+  MuiButton: {
+    styleOverrides: {
+      root: {
+        textTransform: "unset",
+      },
+      contained: {
+        boxShadow: "none",
+        "&:hover": {
+          boxShadow: "none",
+        },
+      },
+    },
+  },
+
+  MuiButtonGroup: {
+    styleOverrides: {
+      root: {
+        boxShadow: "none",
+      },
+    },
+  },
+});
